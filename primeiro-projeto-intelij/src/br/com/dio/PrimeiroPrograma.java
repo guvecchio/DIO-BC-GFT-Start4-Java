8563d65c3aa0114ab1faaac44789f1cefd0c7c0c
guvecchio <luis17ads@gmail.com>
2022-02-22 03:53:44 -0300

    Primeiro Programa IntelliJ

--- primeiro-projeto-intelij/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro-projeto-intelij/src/br/com/dio/PrimeiroPrograma.java
@@ -4,12 +4,50 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        Livro livro1 = new Livro("O Problema dos 3 corpos", 300);
+        System.out.println(livro1);
+
         /*int a = 5;
         int b = 5;
               System.out.println("Olá Mundo!" + (a+b));*/
+    }
+}
 
-        Gato gato = new Gato();
+class Livro{
 
-        System.out.println(gato);
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
